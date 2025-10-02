e05a4952850af1839e8f8573220d9c3c92c3b0df
Elias Matos <ematos@id.uff.br>
2025-10-02 19:32:42 -0300

    Limpando...

--- FRONT-END/Projeto de Bloco (FE com Framework)/AT/React Native/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const SUPABASE_URL = "https://ypqinedsahltuzurrfhq.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwcWluZWRzYWhsdHV6dXJyZmhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMDAzNzIsImV4cCI6MjA3Mzc3NjM3Mn0.mzXh8RMv8Luj6aPmEoNCC4hp3up3meC1iCcPIu65SXY";
-
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: false }
-});
